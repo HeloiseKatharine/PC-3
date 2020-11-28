1b544e5aa2541b150b0a0a23d84bf419bd2e721e
Heloise Katharine <katharineheloise@gmail.com>
2020-11-28 18:50:00 -0300

    Questão 2

---
+++ Trabalho1/src/trabalho1/Questao2.java
@@ -0,0 +1,13 @@
+/*
+2. Faça um ArrayList e insira as Strings “String 1”, “String 2” e “String 3” cada uma duas vezes, percorra todos 
+os elementos e imprima (System.out.println()). Observe o resultado. Quantos elementos têm a coleção?
+ */
+package trabalho1;
+
+/**
+ *
+ * @author Heloise
+ */
+public class Questao2 {
+    
+}
