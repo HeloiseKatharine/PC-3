8127a3aab2558bfe9f7d8bb20e9612ce89fad372
Heloise Katharine <katharineheloise@gmail.com>
2020-11-28 23:02:11 -0300

    Questão 5

---
+++ Trabalho1/src/trabalho1/Questao5.java
@@ -0,0 +1,41 @@
+package trabalho1;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/*
+5. Utilizando a classe Collections ordene a coleção criada no exercício 2. Imprima (System.out.println()) o resultado.
+ */
+
+/**
+ *
+ * @author Heloise 
+ */
+public class Questao5 {
+     public static void main(String[] args) {
+     
+        List lista = new ArrayList();
+        lista.add("String 1");
+        lista.add("String 2");
+        lista.add("String 3");
+        
+        lista.add("String 1");
+        lista.add("String 2");
+        lista.add("String 3");
+        
+         System.out.println("Lista: ");
+        for(int i=0; i<lista.size(); i++){
+            System.out.println(lista.get(i));
+        }
+        
+        System.out.println("Lista Ordenada: ");
+        
+        Collections.sort(lista);
+        for(int i=0; i<lista.size(); i++){
+            System.out.println(lista.get(i));
+        }
+    }
+}
+
