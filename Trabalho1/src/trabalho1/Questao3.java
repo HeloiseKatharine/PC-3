87d4515bafe0be9467c5ac1f0b96d3a9f0cd83cb
Heloise Katharine <katharineheloise@gmail.com>
2020-11-28 22:54:58 -0300

    Questão 3

---
+++ Trabalho1/src/trabalho1/Questao3.java
@@ -0,0 +1,42 @@
+/*
+3. Utilizando a classe Collections desordene a coleção criada no exercício 2. Imprima (System.out.println()) o resultado.
+ */
+package trabalho1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ *
+ * @author Heloise
+ */
+public class Questao3 {
+    public static void main(String[] args) {
+       
+        List lista = new ArrayList();
+        lista.add("String 1");
+        lista.add("String 2");
+        lista.add("String 3");
+        
+        lista.add("String 1");
+        lista.add("String 2");
+        lista.add("String 3");
+        
+        System.out.println("Lista ordenada");
+        for(int i=0; i<lista.size(); i++){
+            System.out.println(lista.get(i));
+        }
+
+        /*Lista não ordenada*/
+        //Collections.shuffle(lista);
+        
+        
+        Collections.reverse(lista);
+        
+        System.out.println("Lista invertida");
+         for(int i=0; i<lista.size(); i++){
+            System.out.println(lista.get(i));
+        }
+    }
+}
