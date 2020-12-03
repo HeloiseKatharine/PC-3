e02777c76068299d0c21996edff92508495a2f48
Heloise Katharine <katharineheloise@gmail.com>
2020-12-02 23:07:30 -0300

    Questão 12

---
+++ Trabalho1/src/trabalho1/Questao12.java
@@ -0,0 +1,16 @@
+/*
+Porque as médias encontradas nos exercícios 10 e 11 ficaram diferentes?
+ */
+package trabalho1;
+
+/**
+ *
+ * @author Heloise
+ */
+public class Questao12 {
+    /*
+    Porque as médias encontradas nos exercícios 10 e 11 ficaram diferentes?
+    
+    Os resultados foram diferentes pois Set não permite elementos duplicados.
+    */
+}
