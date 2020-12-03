4cb44887564ce63a315f3e571d48512ad743111b
Heloise Katharine <katharineheloise@gmail.com>
2020-12-02 22:40:18 -0300

    Questão 11

--- Trabalho1/src/trabalho1/Questao6.java
+++ Trabalho1/src/trabalho1/Questao6.java
@@ -1,14 +1,19 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+6. Crie uma classe chamada Servico que tenha os seguintes atributos: codigo, descricao, precoHora, qtdeHora. 
+Deve-se criar um método que calcule o valor do serviço da seguinte forma: valor = precoHora*qtdHora. O preço
+da hora de serviço deve ser atribuído no momento da criação do objeto da classe. Sobrescreva o método
+compareTo para fazer a ordenação pela descricao. Crie, também, uma classe usuária de Servico que leia vários
+serviços, até o usuário decida parar de digitar, e guarde-os em uma coleção e no final imprima a lista na ordem
+que foi digitada e na ordem alfabética da Descrição.
  */
 package trabalho1;
 
 /**
  *
- * @author heloh
+ * @author Heloise
  */
 public class Questao6 {
-    
+    public static void main(String[] args) {
+        
+    }
 }
