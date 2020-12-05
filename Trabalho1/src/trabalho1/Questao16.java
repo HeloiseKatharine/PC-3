5bafefba108065590f737ea89982ea88bab7d3e5
Heloise Katharine <katharineheloise@gmail.com>
2020-12-04 23:46:58 -0300

    Questão 16

--- Trabalho1/src/trabalho1/Questao16.java
+++ Trabalho1/src/trabalho1/Questao16.java
@@ -14,20 +14,25 @@ import java.util.Set;
 public class Questao16 {
     public static void main(String[] args) {
         
-        Set<String> contas = new HashSet<>();
+        Set<ContaCorrente> contas = new HashSet<>();
         
-        contas.add("321876");
-        contas.add("654545");
-        contas.add("354545");
-        contas.add("655445");
-        contas.add("358795");
+       /* public ContaCorrente(String agencia, Integer numero, String nome, String cpf, String saldo) */
+            
+        ContaCorrente conta1 = new ContaCorrente("dsd", 212021, "Heloise", "2121212154", "150,00");
+        ContaCorrente conta2 = new ContaCorrente("dsd", 454545, "Zahid", "2121212154", "150,00");
+        ContaCorrente conta3 = new ContaCorrente("dsd", 215461, "joão", "2121212154", "150,00");
+        ContaCorrente conta4 = new ContaCorrente("dsd", 254561, "Maria", "2121212154", "150,00");
+        ContaCorrente conta5 = new ContaCorrente("dsd", 287545, "MariaDB", "2121212154", "150,00");
         
-        System.out.println(contas);
+        contas.add(conta1);
+        contas.add(conta2);
+        contas.add(conta3);
+        contas.add(conta4);
+        contas.add(conta5);
         
-       /* Iterator<String> it = contas.iterator();
+        Iterator<ContaCorrente> it = contas.iterator();
         while(it.hasNext()){
             System.out.println(it.next());
-        }*/
-        
+        }
     }
 }
