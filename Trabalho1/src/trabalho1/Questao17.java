8dd6c64313e43de11f02810d526f9cdd23a44441
Heloise Katharine <katharineheloise@gmail.com>
2020-12-04 23:48:01 -0300

    Questão  17

---
+++ Trabalho1/src/trabalho1/Questao17.java
@@ -0,0 +1,18 @@
+/*
+17. Qual interface do Framework Collections você utilizaria para representar uma Turma de faculdade,
+considerando que cada aluno está matriculado apenas uma vez?
+ */
+package trabalho1;
+
+/**
+ *
+ * @author Heloise
+ */
+public class Questao17 {
+    /*
+    17. Qual interface do Framework Collections você utilizaria para representar uma Turma de faculdade,
+    considerando que cada aluno está matriculado apenas uma vez?
+    
+       Set, pois não permite elementos duplicados.
+    */
+}
