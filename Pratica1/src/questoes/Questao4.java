9caf8e6a27ba4753ceb305de5d3e9068de6aba44
Heloise Katharine <katharineheloise@gmail.com>
2020-12-13 12:41:45 -0300

    Questão 4

---
+++ Pratica1/src/questoes/Questao4.java
@@ -0,0 +1,15 @@
+/*
+4)Qual é a diferença fundamental entre a classe Vector e a classe ArrayList do pacote java.util?
+ */
+package questoes;
+
+/**
+ *
+ * @author Heloise
+ */
+public class Questao4 {
+    /* 
+    Vector é sincronizado, ArrayList não é.
+    A alocação dinâmica do ArrayList aumenta em 50% e a do Vector aumenta o dobro.
+    */
+}
