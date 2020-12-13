bf4280a8d70c31ec502e1576a4aa3bfe66aea2ee
Heloise Katharine <katharineheloise@gmail.com>
2020-12-13 12:41:39 -0300

    Questão 5

---
+++ Pratica1/src/questoes/Questao5.java
@@ -0,0 +1,19 @@
+/*
+5) Qual é a diferença fundamental entre a classe Hashtable e a classe HashMap do pacote java.util?
+ */
+package questoes;
+
+/**
+ *
+ * @author Heloise
+ */
+public class Questao5 {
+    /*
+    HashTable e HashMap , bascicamente tem o mesmo comportamento , a única diferença é que o primeiro é thread-safe e o outro não.
+
+    Por thread-safe , vc pode entender como , se várias threads vão acessar e/ou modificar o objeto,
+    e para que isso seja feito sem que uma thread interfira em outra , é preciso um tratamento de concorrência 
+    que contorna este problema , mas impacta na performance.
+    */
+    
+}
