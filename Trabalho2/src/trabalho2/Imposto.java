acf7c093026871aced21406f21fd962ddf1c6293
Heloise Katharine <katharineheloise@gmail.com>
2021-01-02 23:21:34 -0300

    Imposto

---
+++ Trabalho2/src/trabalho2/Imposto.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalho2;
+
+/**
+ *
+ * @author Heloise
+ */
+public class Imposto {
+
+    public static double calcularImposto(double valor, Taxa tipo) {
+        switch (tipo) {
+            case IPTU:
+                return valor * Taxa.IPTU.getPercentual();
+            case IPVA:
+                return valor * Taxa.IPVA.getPercentual();
+            case ITBI:
+                return valor * Taxa.ITBI.getPercentual();
+            case ISSQN:
+                return valor * Taxa.ISSQN.getPercentual();
+            default:
+                return 0.0;
+        }
+    }
+}
