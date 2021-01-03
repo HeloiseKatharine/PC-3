0ae620e9a0c4d8a91b390ade7ded0e13de45bf2b
Heloise Katharine <katharineheloise@gmail.com>
2021-01-02 23:21:44 -0300

    Oficina

---
+++ Trabalho2/src/trabalho2/Oficina.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalho2;
+
+/**
+ *
+ * @author Heloise
+ */
+public enum Oficina {
+    
+    CARGADECARTUCHO("Carga de cartucho", 25.00),
+    CONSERTODEIMPRESSORA("Conserto de impressora", 100.00),
+    CONSERTODECOMPUTADORES("Conserto de computadores", 150.00),
+    TROCADEBATERIA("Troca de bateria", 100.00),
+    CONSERTODECARREGADOR("Conserto de carregador", 75.00);
+
+}
