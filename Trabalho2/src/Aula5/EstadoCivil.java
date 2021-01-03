809c5dab4b63dc5265f8e2d475c233d1e15c7d9d
Heloise Katharine <katharineheloise@gmail.com>
2021-01-03 02:01:48 -0300

    EstadoCivil

---
+++ Trabalho2/src/Aula5/EstadoCivil.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aula5;
+
+/**
+ *
+ * @author Heloise
+ */
+public enum EstadoCivil {
+    SOLTEIRO(1), CASADO(2), DIVORCIADO(3), VIUVO(4), OUTRO(5);
+
+    private int valor;
+
+    private EstadoCivil(int valor) {
+        this.valor = valor;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+}
