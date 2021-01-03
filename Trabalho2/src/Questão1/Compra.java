dbc43e545b5ab4e2521f4ce5623f3f72293ad262
Heloise Katharine <katharineheloise@gmail.com>
2021-01-03 19:45:21 -0300

    Compra

---
+++ Trabalho2/src/Questão1/Compra.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Questão1;
+
+/**
+ *
+ * @author heloh
+ */
+public class Compra extends Thread {
+
+    private Produto produto;
+    private double quantia;
+    private static final int REPETICOES = 10;
+    private static final int ESPERA = 10;
+
+    public Compra(Produto produto, double quantia) {
+        this.produto = produto;
+        this.quantia = quantia;
+    }
+
+    public void run() {
+        try {
+            for (int i = 1; i <= REPETICOES && !isInterrupted(); i++) {
+                produto.compra(quantia);
+                sleep(ESPERA);
+            }
+        } catch (InterruptedException ie) {
+            System.out.println(ie.getMessage());
+        }
+    }
+}
