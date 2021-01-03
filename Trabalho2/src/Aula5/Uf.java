61c06c3ec7680186faa2159c1cfff638cf5c63ac
Heloise Katharine <katharineheloise@gmail.com>
2021-01-03 02:03:54 -0300

    Uf

---
+++ Trabalho2/src/Aula5/Uf.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Aula5;
+
+/**
+ *
+ * @author Heloise
+ */
+public enum Uf {
+    AC("Acre"),
+    AL("Alagoas"),
+    AP("Amapá"),
+    AM("Amazonas"),
+    BA("Bahia"),
+    CE("Ceará"),
+    DF("Distrito Federal"),
+    MS("Mato Grosso do Sul"),
+    MT("Mato Grosso"),
+    GO("Goiás"),
+    RO("Rondônia"),
+    TO("Tocantins"),
+    RN("Rio Grande do Norte"),
+    SP("São Paulo"),
+    RJ("Rio de Janeiro"),;
+
+    private String descricao;
+
+    private Uf(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
